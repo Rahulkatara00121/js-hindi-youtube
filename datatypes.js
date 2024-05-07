ca80720074656e739e9dc947fabdf60454b9fe4c
Rahulkatara00121 <128904714+Rahulkatara00121@users.noreply.github.com>
2024-05-07 07:55:06 +0000

    datatypes change

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+// primitives
+
+// 7 types : Sting , number , boolean , null, undifined , symbol , bigint
+
+
+// Refremce (Non Primitives)
+
+// Array , objects, function
+
+
+// javascript is Dynamiclly typed because same variable can store Diffrent datatypes
+
+    const Id = Symbol('123')
+    const anotherId = Symbol('123')
+
+    console.log(Id === anotherId);
