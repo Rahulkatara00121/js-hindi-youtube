e1851a7bba93588609fb78f8282e852016bbb0b3
Rahulkatara00121 <128904714+Rahulkatara00121@users.noreply.github.com>
2024-05-08 12:23:56 +0000

    Nothing

---
+++ time.js
@@ -0,0 +1,11 @@
+let myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toLocaleString())
+console.log(myDate); 
+
+const marvalHero = ["ironman", "captaionAmerica","loki"]
+
+const dcHero = ["flash","batman", "superhero"]
+
+const allhero = marvalHero.concat(dcHero)
+console.log(allhero);
