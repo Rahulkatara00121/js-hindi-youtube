a647cd23cc9f0b7e1d9fc0c05c3792ce9f658c3c
Rahulkatara00121 <128904714+Rahulkatara00121@users.noreply.github.com>
2024-05-09 13:38:04 +0000

    Day5

---
+++ 02_objects.js
@@ -0,0 +1,32 @@
+// Two ways to make object
+// singalton (when object made by constructor)
+
+//object literals
+const mySum = Symbol("key1")
+
+const jsUser = {
+    name : "Rahul ",
+    "surName" : "katara",
+    [mySum]:"mykey1"
+}
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"c",4:"d"}
+
+//const obj3 =  Object.assign({}, obj1 , obj2)
+const obj3 = {...obj1,...obj2}
+//console.log(obj3);
+
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor: "hitesh"
+}
+
+const {courseInstructor} = course
+
+//console.log(courseInstructor);
+
+
+
+
